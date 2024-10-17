507c2ade1821aca3824fadebaf28d1e78259e775
Galaxy-104 <js_park95@naver.com>
2024-10-17 17:31:55 +0900

    feat: 로그인 기능 구현
    body로 입력받은 email과 password로 회원 인증하는 기능 구현

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,11 @@
 import { Router, json } from 'express';
-import { join } from '../controllers/UserController';
+import { join, login } from '../controllers/UserController';
 
 const router: Router = Router();
 
 router.use(json());
 
 router.post('/join', join);
+router.post('/login', login);
 
 export default router;
