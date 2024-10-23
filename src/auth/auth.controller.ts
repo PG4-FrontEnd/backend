06b9313fd74b2d948ff7bb854606ca21726c59b4
Galaxy-104 <js_park95@naver.com>
2024-10-23 12:40:09 +0900

    feat: 로그인 기능 구현
    로그인 성공시 유저의 정보를 쿠키로 전달

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Request, Response, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/layer/users/user.dto';
 
@@ -10,4 +10,22 @@ export class AuthController {
   async register(@Body() userDto: CreateUserDto) {
     return this.authService.register(userDto);
   }
+
+	@Post('login')
+	async login(@Request() req, @Response() res) {
+		const userInfo = await this.authService.validateUser(
+			req.body.email,
+			req.body.password,
+		);
+
+		if (userInfo) {
+			res.cookie('login', JSON.stringify(userInfo), {
+				httpOnly: false,
+				maxAge: 1000 * 60 * 60 * 24
+			})
+
+			return res.status(HttpStatus.OK).end()
+		}
+
+	}
 }
