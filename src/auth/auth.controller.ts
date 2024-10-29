a10e35c0867e70b2633be8708b1dfef2996f72cd
Galaxy-104 <js_park95@naver.com>
2024-10-29 10:29:02 +0900

    remove : 사용하지 않는 파일 및 코드 제거

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Post, Body, Request, Response, HttpStatus, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from 'src/layer/users/user.dto';
-// import { LoginGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -22,21 +21,4 @@ export class AuthController {
 
 	}
 
-	// @UseGuards(LoginGuard)
-	// @Post('login2')
-	// async login2(@Request() req, @Response() res) {
-	// 	if (!req.cookies['login'] && req.user) {
-	// 		res.cookie('login', JSON.stringify(req.user), {
-	// 			httpOnly: true,
-	// 			maxAge: 1000 * 60 * 60 * 24,
-	// 		});
-	// 	}
-	// 	return res.send({ message: 'login2 success'})
-	// }
-
-	// @UseGuards(LoginGuard)
-	// @Get('test-guard')
-	// testGuard() {
-	// 	return '로그인 시에만 보임';
-	// }
 }
